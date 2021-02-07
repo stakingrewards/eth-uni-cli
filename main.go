f6ed15b1d7238243dd3f09fc78c9b701c49af70b
Javier Bullrich <javier@bullrich.dev>
2021-02-07 01:36:56 +0100

    Added balance endpoint
    Added endpoint to return balance with the coins pre defined in wallet

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 
 	"github.com/Bullrich/eth-uni-cli/wallet"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	printBalances()
+	startWebServer()
 }
 
 func printBalances() {
@@ -18,4 +20,27 @@ func printBalances() {
 	client := wallet.NewUser(apiKey, walletAddress)
 
 	fmt.Println(client.GetBalances())
+}
+
+func startWebServer() {
+	app := fiber.New()
+
+	apiKey := os.Getenv("INFURA_API_KEY")
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+
+	app.Get("/balance/:address", func(c *fiber.Ctx) error {
+		user := wallet.NewUser(apiKey, c.Params("address"))
+		balance := user.GetBalances()
+		balanceJSON, err := json.Marshal(balance)
+		if err != nil {
+			return c.SendStatus(400)
+		}
+
+		return c.SendString(string(balanceJSON))
+	})
+
+	app.Listen(":3000")
 }
