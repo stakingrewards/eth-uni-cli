1fc3e074d0c63a4673c877760f7d4c2d147fb1cb
Javier Bullrich <javier@bullrich.dev>
2021-02-07 17:08:36 +0100

    Improved error checking in uniswap module
    Improved the module of uniswap to look for more errors and return them on invalid cases instead of panicking

--- wallet/uniswap.go
+++ wallet/uniswap.go
@@ -6,35 +6,37 @@ import (
 	"errors"
 	"fmt"
 	"github.com/Bullrich/eth-uni-cli/uniswap"
-	"github.com/Bullrich/eth-uni-cli/utils"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
-	"log"
 	"math/big"
 	"time"
 )
 
 const uniswapRouterAddress = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
 
-func (u *User) obtainContractsRequirements(privateWalletKey string, weiToUse int64) *bind.TransactOpts {
+func (u *User) obtainContractsRequirements(privateWalletKey string, weiToUse int64) (*bind.TransactOpts, error) {
 	privateKey, err := crypto.HexToECDSA(privateWalletKey)
-	utils.CheckError(err)
+	if err != nil {
+		return nil, err
+	}
 
 	publicKey := privateKey.Public()
 	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
 	if !ok {
-		log.Fatal("error casting public key to ECDSA")
+		return nil, errors.New("error casting public key to ECDSA")
 	}
 
 	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
 
 	nonce, err := u.client.PendingNonceAt(context.Background(), fromAddress)
-	utils.CheckError(err)
-
+	if err != nil {
+		return nil, err
+	}
 	gasPrice, err := u.client.SuggestGasPrice(context.Background())
-	utils.CheckError(err)
-
+	if err != nil {
+		return nil, err
+	}
 	auth := bind.NewKeyedTransactor(privateKey)
 
 	auth.Nonce = big.NewInt(int64(nonce))
@@ -42,7 +44,7 @@ func (u *User) obtainContractsRequirements(privateWalletKey string, weiToUse int
 	auth.GasLimit = uint64(300000)    // in units
 	auth.GasPrice = gasPrice
 
-	return auth
+	return auth, nil
 }
 
 func BuyCoin(user *User, privateKey string, coinName string, input int64, output int64) (string, error) {
@@ -51,11 +53,14 @@ func BuyCoin(user *User, privateKey string, coinName string, input int64, output
 		return "", err
 	}
 
-	auth := user.obtainContractsRequirements(privateKey, input)
+	auth, err := user.obtainContractsRequirements(privateKey, input)
+	if err != nil {
+		return "", err
+	}
 
 	coinContract := common.HexToAddress(coinAddress)
 
-	transactionHex := user.buyCoin(coinContract, output, auth)
+	transactionHex, err := user.buyCoin(coinContract, output, auth)
 	return transactionHex, err
 }
 
@@ -69,26 +74,23 @@ func getCoinAddress(coinName string) (string, error) {
 		return saiContract, nil
 	}
 
-	return "", errors.New(fmt.Sprintf("Coin %v not valid. Use 'dai', 'mkr' or 'sai'", coinName))
+	return "", errors.New(fmt.Sprintf("Coin '%v' not valid. Use 'dai', 'mkr' or 'sai'", coinName))
 }
 
-func BuyDai(user *User, privateKey string) {
-	auth := user.obtainContractsRequirements(privateKey, 10000000000000000)
-
-	address := common.HexToAddress(daiContract)
-
-	user.buyCoin(address, 1560000, auth)
-}
-
-func (u *User) buyCoin(coinAddress common.Address, amountToBuy int64, auth *bind.TransactOpts) string {
+func (u *User) buyCoin(coinAddress common.Address, amountToBuy int64, auth *bind.TransactOpts) (string, error) {
 	contractAddress := common.HexToAddress(uniswapRouterAddress)
 	uniswapContract, err := uniswap.NewUniswap(contractAddress, u.client)
-	utils.CheckError(err)
+	if err != nil {
+		return "", err
+	}
 
 	amountOut := big.NewInt(amountToBuy)
 	callOptsParams := &bind.CallOpts{Pending: true}
 	weth, err := uniswapContract.WETH(callOptsParams)
-	utils.CheckError(err)
+	if err != nil {
+		return "", err
+	}
+
 	addresses := []common.Address{
 		weth,
 		coinAddress,
@@ -98,9 +100,11 @@ func (u *User) buyCoin(coinAddress common.Address, amountToBuy int64, auth *bind
 	i := new(big.Int).SetInt64(now + (time.Second.Milliseconds() * 40))
 
 	tx, err := uniswapContract.SwapETHForExactTokens(auth, amountOut, addresses, u.address, i)
-	utils.CheckError(err)
+	if err != nil {
+		return "", err
+	}
 
 	fmt.Printf("tx sent. See the transaction in: https://etherscan.io/tx/%s\n", tx.Hash().Hex())
 
-	return tx.Hash().Hex()
+	return tx.Hash().Hex(), nil
 }
